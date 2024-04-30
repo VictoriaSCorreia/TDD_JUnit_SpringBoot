14db17801534f107a0c2f9b4a21cc73e250e4240
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-04-30 13:26:16 -0300

    Update

---
+++ src/main/java/com/project/meetupManager/repository/RegistrationRepository.java
@@ -0,0 +1,13 @@
+package com.project.meetupManager.repository;
+
+import com.project.meetupManager.model.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
+
+    boolean existsByRegistration(String registration);
+
+    Optional<Registration> findByRegistration(String registrationAtrb);
+}
