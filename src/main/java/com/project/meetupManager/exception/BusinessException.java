a78cd43f776eeef313c6049ed20517e48578c197
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-05-01 15:05:54 -0300

    new update

---
+++ src/main/java/com/project/meetupManager/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.project.meetupManager.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/project/meetupManager/service/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.project.meetupManager.service;
+
+import com.project.meetupManager.model.Registration;
+
+public interface RegistrationService {
+
+    Registration save(Registration any);
+}
