3b160b470486750277d7bef079dcdb98eca69821
Victoria Correia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-07-26 22:55:47 -0300

    Update RegistrationRepository.java

--- src/main/java/com/project/meetupManager/repository/RegistrationRepository.java
+++ src/main/java/com/project/meetupManager/repository/RegistrationRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
     boolean existsByRegistration(String registration);
-
     Optional<Registration> findByRegistration(String registrationAtrb);
 }
